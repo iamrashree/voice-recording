c0a02991ee87ba9d28136380b49e51643539952f
Rajashree <rajashree@udmglobal.com>
2022-12-29 10:07:31 +0530

    voice recording

--- src/app/voice-record/voice-record.component.ts
+++ src/app/voice-record/voice-record.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-voice-record',
@@ -7,9 +7,79 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VoiceRecordComponent implements OnInit {
 
+  public recordOwnVoiceStarted = false;
+  public playRecording = false;
+  public audioCtx = new AudioContext();
+  public blob: any;
+  public source: any;
+  public audio: any;
+  public record: any;
+
+  @ViewChild('recordedAudio')
+  public recordedAudio!: ElementRef;
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  start() {
+    navigator.mediaDevices.getUserMedia({ audio: true })
+    .then(stream => {
+      const audioChunks: any[] = [];
+      this.record = new MediaRecorder(stream);
+
+      this.record.ondataavailable = async (e: any) => {
+        audioChunks.push(e.data);
+        console.log('audioChunks', audioChunks);
+        if (this.record.state == "inactive") {
+          this.blob = new Blob(audioChunks, { type: 'audio/x-mpeg-3' });
+          this.recordedAudio.nativeElement.src = URL.createObjectURL(this.blob);
+        }
+      }
+      this.record.start();
+    })
+    .catch(e => console.log(e));
+  }
+
+  stop() {
+    this.record.stop();
+  }
+
+  public async getData() {
+    const offlineCtx = new OfflineAudioContext(2,44100*40,44100);
+    this.source = this.audioCtx.createBufferSource();
+
+    const audioData = await this.blob.arrayBuffer();
+
+    offlineCtx.decodeAudioData(audioData, (buffer) => {
+      this.source.buffer = buffer;
+      this.source.playbackRate.value = 1;
+      this.source.connect(this.audioCtx.destination);
+    });
+  }
+
+  recordVoice() {
+    if(!this.recordOwnVoiceStarted) {
+      this.start();
+    } else {
+      this.stop();
+    }
+    this.recordOwnVoiceStarted = !this.recordOwnVoiceStarted;
+  }
+
+  play() {
+    if (!this.playRecording) {
+      this.getData();
+      this.source.start(0);
+      
+    } else {
+      this.source.stop(0);
+    }
+    this.playRecording = !this.playRecording;
+  }
+
+  // pause() {
+  //   this.source.stop(0);
+  // }
 }
